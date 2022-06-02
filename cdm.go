ad08e7e2e747511cf114cb51e942213e2183f166
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2022-06-01 20:14:23 -0600

    Fixed errors in init function, and switched to returning errors

--- cdm.go
+++ cdm.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
-
-	"gorm.io/gorm"
 )
 
-var dbSQLite bool
 var dbPostGres bool
 var dbPostGresDSN string
-var openFile string
+
+var projectDirectory string
 
 var debugLogger = log.New(ioutil.Discard, "DEBUG: ", 0)
 var infoLogger = log.New(os.Stderr, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
@@ -33,72 +33,35 @@ func main() {
 	// Calculate conduit / cable tray fill
 	// Print out layout diagram of individual or all locations
 
-	var dbSL *gorm.DB
-	var dbPG *gorm.DB
-	var dbLocalSL *gorm.DB
+	//var dbPG *gorm.DB
 	var err error
 	err = initialization()
 	if err != nil {
 		fatalLogger.Panicln("Config and flag init failed", err)
 	}
-	// connect to databases
-	if dbSQLite {
+	// connect to database
 
-		if dbSL, err = connect("SQLite", ""); err != nil {
-			fatalLogger.Panicln("Failed to initialize SQLite database", err)
-		} else {
-			// cannot close database connections created with GORM
-			// in the normal way
-			//	defer dbSL.Close()
-		}
-	}
-	if dbPostGres {
+	//if dbPostGres {
 
-		if dbPG, err = connect("PGSQL", dbPostGresDSN); err != nil {
-			fatalLogger.Panicln("Failed to initialize PostGreSQL database", err)
-		} else {
-			// cannot close database connections created with GORM
-			// in the normal way
-			//	defer dbPG.Close()
+	//	if dbPG, err = connect("PGSQL", dbPostGresDSN); err != nil {
+	//		fatalLogger.Panicln("Failed to initialize PostGreSQL database", err)
+	//	} else {
+	//		// cannot close database connections created with GORM
+	//		// in the normal way
+	//		//	defer dbPG.Close()
 
-		}
-	}
-	if openFile != "" {
+	//	}
+	//}
 
-		if dbLocalSL, err = connect("SQLite", path.Base(openFile)); err != nil {
-			fatalLogger.Panicf("failed to open local project %s. Error: %s", openFile, err)
-		} else {
-			// cannot close database connections created with GORM
-			// in the normal way
-			//	defer dbLocalSL.Close()
-
-		}
-	}
+	//
 }
 
 //set up command line options, logging and configuration
-//read config file, either from -c flag or default in ~/.config
 func initialization() error {
-
-	//if this fails, cannot create default configDir. This is fatal
-	//currUserConfigDir, err := os.UserConfigDir()
-	//if err != nil {
-	//	return err
-	//}
-
-	//default paths. Will not be overridden
-	//defaultConfigPath := path.Join(currUserConfigDir, "cdm.cfg")
 
 	//Define and parse commandline flags here
 	//Defaults are set in flags as appropriate
-	//flagConfigPath := flag.String("c", defaultConfigPath, "Path to config file")
 
-	//project file is a folder containing a sqlLite db
-	//and graphviz files, as well as documentation and
-	//local config files
-	// SQLite file:= {projectfile}/{projectfile}.sqlite
-	flagOpenFile := flag.String("o", "", "open project `directory`")
-	flagDBSqlLite := flag.Bool("s", false, "Use project specific SqlLite database")
 	flagDBPostGres := flag.Bool("p", false, "Use PostgreSQL database")
 	flagDBPostGresDSN := flag.String("pgDSN", "", "Data Source Name (DSN) for PostGres database connection")
 	flagDebugLogging := flag.Bool("V", false, "Show debug logs")
@@ -121,27 +84,39 @@ func initialization() error {
 	}
 
 	//retrieve values from flags and set global variables
-	dbSQLite = *flagDBSqlLite
 	dbPostGres = *flagDBPostGres
 	dbPostGresDSN = *flagDBPostGresDSN
-	openFile = *flagOpenFile
 
-	//if dBSqlLite && dBPostGres {
-	//	fatalLogger.Panicln("Multiple databases selected. This is not supported. Fix invocation")
-	//}
+	if len(flag.Args()) > 1 {
+		infoLogger.Println("Multiple project directories specified, only using first")
+	}
 
-	//if *flagConfigPath != defaultConfigPath {
-	//	infoLogger.Println("Using config file path from flag", *flagConfigPath)
-	//}
+	projectDirectory = flag.Arg(0) // first remaining cmd line arg after flag processing
 
-	//Attempt to read config
-	//tempConfig, cfgErr := readConfig(*flagConfigPath)
-	//if cfgErr != nil {
-	//	infoLogger.Printf("Config file not openable at path: %s, Err: %s "+
-	//		"Using default configuration\n", *flagConfigPath, cfgErr)
-	//} else {
-	//	config = tempConfig
-	//}
+	// check to see if projectDirectory was actually specified
+	if projectDirectory == "" {
+		//TODO: switch to custom error here
+		return errors.New("No project directory specified")
+		//fatalLogger.Fatalln("Please specify a project directory")
+	}
+
+	// check to see if projectDirectory is a valid directory
+
+	if !fs.ValidPath(projectDirectory) {
+		// projectDirectory is not a valid path
+		return &fs.PathError{Op: "check", Path: projectDirectory, Err: fs.ErrInvalid}
+	} else {
+		fileSystem := os.DirFS(projectDirectory)
+		fileInfo, err := fs.Stat(fileSystem, projectDirectory)
+		if err != nil {
+			return err
+		}
+		if !fileInfo.IsDir() {
+			// projectDirectory is not a directory
+			return fmt.Errorf("Specified Project Directory %s is not a directory", projectDirectory)
+		}
+
+	}
 
 	return nil
 
